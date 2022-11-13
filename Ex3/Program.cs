dacf3761625ff115449fbebb4e0f6db10b96f8c6
Vladimir Stepankin <vladimirstepankin@mail.ru>
2022-11-13 12:23:50 +0300

    Добавил третью задачу

---
+++ Ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт,
+ является ли число чётным (делится ли оно на два без остатка)*/
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastDigit = number % 2;
+if (lastDigit == 0)
+{
+    Console.Write("Число " + number + " четное");
+}
+else
+{
+    Console.Write("Число " + number + " нечетное");
+}
