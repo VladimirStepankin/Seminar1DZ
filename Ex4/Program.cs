69d1ef6034e56723d2014360012980f8e6365ba3
Vladimir Stepankin <vladimirstepankin@mail.ru>
2022-11-13 12:50:59 +0300

    Добавил четвертую задачу

---
+++ Ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N*/
+Console.Write("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+if (number > 0)
+{
+    while (a < number)
+    {
+        int b = a % 2;
+        if (b == 0)
+        {
+            Console.Write(a + " ");
+        }
+        a++;
+    }
+}
+else
+{
+    Console.Write("Вы ввели не натуральное число");
+}
